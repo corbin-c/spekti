29dcd9c09e022defb5ee7c0be8ab091323b99540
Clement Corbin <corbin.clement@gmail.com>
2019-08-07 00:46:22 +0200

    data sources handling

---
+++ sources.js
@@ -0,0 +1,10 @@
+function Sources(gist) {
+  this.gist = gist;
+  this.addSource = function(type,url) {
+  };
+  this.removeSource = function(url) {
+  };
+  this.getSources = function() {
+  };
+}
+export { Sources };
