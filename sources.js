0fc24910825cec7719eafbe18e5ebfa14bcc60cb
Clement Corbin <corbin.clement@gmail.com>
2019-08-07 01:19:16 +0200

    getSources from Gist

--- sources.js
+++ sources.js
@@ -1,10 +1,18 @@
+const GIST_DESC = "SpektiGist";
 function Sources(gist) {
   this.gist = gist;
   this.addSource = function(type,url) {
   };
   this.removeSource = function(url) {
   };
-  this.getSources = function() {
+  this.getSources = async function() {
+    let gist_list = await this.gist.listGists();
+    if (gist_list.filter((e) => (e.description == GIST_DESC)).length == 0) {
+      // No SpektiGist has been found, we shall create one
+      this.gist = await gist.createGist("rss_sources","no sources",GIST_DESC);
+    } else {
+      this.gist = await gist.useGist({description:GIST_DESC});
+    }
   };
 }
 export { Sources };
