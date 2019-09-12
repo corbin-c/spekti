e7c7569588efd087e3564cf75121c799055c86fc
Clement Corbin <corbin.clement@gmail.com>
2019-09-13 00:47:31 +0200

    line limit

--- sources.js
+++ sources.js
@@ -11,7 +11,8 @@ let Sources = class {
     let gist_list = await this.gist.listGists();
     if (gist_list.filter((e) => (e.description == GIST_DESC)).length == 0) {
       // No SpektiGist has been found, we shall create one
-      this.gist = await this.gist.createGist("rss_sources","no sources",GIST_DESC);
+      this.gist = await this.gist.createGist(
+        "rss_sources","no sources",GIST_DESC);
     } else {
       this.gist = await this.gist.useGist({description:GIST_DESC});
     }
