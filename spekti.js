8083fe1b65a044dd1d23c9d4fe8a9e66ea57fddb
Clément Corbin <corbin.clement@gmail.com>
2020-07-15 14:32:24 +0200

    bugs fixed

--- spekti.js
+++ spekti.js
@@ -9,17 +9,21 @@ let SpektiEntity = class {
     this.getContent();
   }
   async getContent() {
-    let content = await this.gist.getContent(file);
-    console.info("retrieved "+file+" from gist");
-    this.content = JSON.parse(content);
-    this.ready();
+    try {
+      let content = await this.gist.getContent(this.file);
+      console.info("retrieved "+this.file+" from gist");
+      this.content = JSON.parse(content);
+    } catch {
+      console.warn("file "+this.file+" not found in gist, will create...");
+    }
+    this.isReady();
   }
   async commitChanges() {
-    await this.gist.putContent(file,JSON.stringify(this.content));
+    await this.gist.putContent(this.file,JSON.stringify(this.content));
   }
   get allContent() {
     return new Promise(async (resolve,reject) => {
-      await this.isReady;
+      await this.ready;
       resolve(this.content);
     });
   }
@@ -27,7 +31,7 @@ let SpektiEntity = class {
 let Tags = class extends SpektiEntity {
   async tagArticle(url,...tag) {
     console.info("adding tags "+tag.join(", ")+" to url "+url);
-    let article = await this.allContent.find(e => e.url == url);
+    let article = (await this.allContent).find(e => e.url == url);
     if (typeof article === "undefined") {
       this.content.push({url,tags:[...tag]});
     } else {
@@ -41,7 +45,7 @@ let Tags = class extends SpektiEntity {
   }
   async untagArticle(url,...tag) {
     console.info("removing tags "+tag.join(", ")+" to url "+url);
-    let article = await this.allContent.find(e => e.url == url);
+    let article = (await this.allContent).find(e => e.url == url);
     if (typeof article === "undefined") {
       console.warn("article "+url+" not found, tag deletion aborted");
     } else {
@@ -67,36 +71,36 @@ let Tags = class extends SpektiEntity {
 let Notes = class extends SpektiEntity {
   async makeNote(content,url=false) {
     console.info("adding note");
-    let id = this.nextId;
+    let id = await this.nextId();
     this.content.push(
       {
         content,
         url,
         id,
-        timestamp: (new Date()).toValue(),
+        timestamp: (new Date()).valueOf(),
       }
     );
     await this.commitChanges();
   }
   async editNote(noteId,content) {
     console.info("editing note #"+noteId);
-    let toEdit = await this.allContent.find(e => e.id == noteId)
+    let toEdit = (await this.allContent).find(e => e.id == noteId)
     if (typeof toEdit === "undefined") {
       console.warn("cannot edit note #"+noteId);
     } else {
       toEdit.content = content;
-      toEdit.timestamp = (new Date()).toValue();
+      toEdit.timestamp = (new Date()).valueOf();
       await this.commitChanges();
     }
   }
   async deleteNote(noteId) {
     console.info("removing note #"+noteId);
-    this.content = await this.allContent.filter(e => e.id !== noteId);
+    this.content = (await this.allContent).filter(e => e.id !== noteId);
     await this.commitChanges();
   }
   async nextId() {
     let id = 0;
-    await this.allContent.map(e => {
+    (await this.allContent).map(e => {
       if (e.id > id) {
         id = e.id;
       }
@@ -104,13 +108,13 @@ let Notes = class extends SpektiEntity {
     return id+1;
   }
   async notesAbout(url) {
-    return await this.allContent.filter(e => e.url == url);
+    return (await this.allContent).filter(e => e.url == url);
   }
 }
 let Sources = class extends SpektiEntity {
   async addSource(url) {
     console.info("adding source "+url);
-    let source = await this.allContent.find(e => e == url);
+    let source = (await this.allContent).find(e => e == url);
     if (typeof source === "undefined") {
       this.content.push(url);
       await this.commitChanges();
@@ -118,11 +122,11 @@ let Sources = class extends SpektiEntity {
   }
   async removeSource(url) {
     console.info("removing source "+url);
-    let source = await this.allContent.find(e => e == url);
+    let source = (await this.allContent).find(e => e == url);
     if (typeof source === "undefined") {
       console.warn("article "+url+" not found, tag deletion aborted");
     } else {
-      this.content = await this.allContent.filter(e => e != url);
+      this.content = (await this.allContent).filter(e => e != url);
       await this.commitChanges();
     }
   }
@@ -143,15 +147,12 @@ let Spekti = class {
       let gist_id = await this.gist.createGist(
         this.entities[0].filename,"[]",GIST_DESC);
       this.gist = await this.gist.useGist({id:gist_id});
-      await Promise.all(this.entities.slice(1).map(async (entity) => {
-        await this.gist.putContent(entity.filename,"[]");
-      }));
     } else {
       this.gist = await this.gist.useGist({description:GIST_DESC});
     }
     console.info("connected to "+GIST_DESC+" gist");
-    this.entities = this.entities.map(e => {
-      return new ENTITIES[e.type](this.gist,e.filename);
+    this.entities.map(e => {
+      this[e.filename] = new ENTITIES[e.type](this.gist,e.filename);
     })
     this.isReady();
   }
