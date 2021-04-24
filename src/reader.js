29cea04ce08ae4a06ed4556e213af9a69692cdd5
Clément Corbin <corbin.clement@gmail.com>
2021-04-24 11:00:22 +0200

    mv

---
+++ src/reader.js
@@ -0,0 +1,10 @@
+import { Readability } from "https://corbin-c.github.io/readability/Readability.js";
+const CORS_PROXY = "https://spekticors.herokuapp.com/";
+let Reader = async function(url) {
+  let doc = await fetch(CORS_PROXY+url);
+  doc = await doc.text();
+  doc = new DOMParser().parseFromString(doc, "text/html");
+  doc.originURI = url;
+  return new Readability(doc).parse();
+}
+export { Reader };
