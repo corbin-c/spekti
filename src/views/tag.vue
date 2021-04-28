9652d07f4c29ba5712501c8180c8ad4b6a078046
Clément Corbin <corbin.clement@gmail.com>
2021-04-28 16:01:36 +0200

    router & store

--- src/views/tag.vue
+++ src/views/tag.vue
@@ -1,77 +1,39 @@
 <template>
   <section class="mt-3 mb-5">
-    <article-view v-if="viewArticle !== false" v-bind:content="viewArticle" v-bind:fullContent="(viewArticle !== false)"></article-view>
-    <div v-else class="card mb-3">
+    <div class="card mb-3">
       <h3 class="card-header card-title">Articles tagged "{{ tag }}"</h3>
-      <p class="card-body card-text"  v-if="taggedContent.length == 0">There is no content for this tag yet. Start reviewing or tagging articles to see something here.</p>
+      <p class="card-body card-text"  v-if="$store.getters.taggedContent(tag).length == 0">There is no content for this tag yet. Start reviewing or tagging articles to see something here.</p>
       <ul class="list-group list-group-flush" v-else>
-        <li class="list-group-item d-flex justify-content-between align-items-center" v-for="article in taggedContent" :key="article.url" v-on:click="showArticle">
-          {{ article.title }}
-          <span class="d-none">{{ article.url }}</span>
-          <span class="badge badge-info badge-pill" v-for="otherTag in otherTags(article)" :key="otherTag" v-on:click="changeTag(otherTag)">{{ otherTag }}</span>
+        <li class="list-group-item d-flex justify-content-between align-items-center" v-for="article in $store.getters.taggedContent(tag)" :key="article.url">
+          <router-link
+            class="text-secondary text-decoration-none"
+            :to="article.url"
+            :title="'Read this article: '+article.title">
+              {{ article.title }}
+          </router-link>
+          <router-link
+            class="badge badge-info badge-pill"
+            v-for="otherTag in $store.getters.otherTags(article.url,tag)"
+            :key="otherTag"
+            :title="'View more articles with this tag: '+otherTag"
+            :to="'/tag/'+otherTag">{{ otherTag }}</router-link>
+          <button title="untag this article" class="badge-delete badge badge-pill" v-on:click="removeTag(article.url)">
+            <svg class="d-inline">
+              <use xlink:href="/octicons-sprite/octicons-sprite.svg#trashcan-16"></use>
+            </svg>
+          </button>
         </li>
       </ul>
     </div>
   </section>
 </template>
 <script>
-import articleView from "./article.vue";
 
 export default {
-  data: function() {
-    return {
-      allTags: [],
-      viewArticle: false,
-    }
-  },
-  watch: {
-    update: function () {
-      this.getTags();
-    }
-  },
-  computed: {
-    taggedContent() {
-      return this.allTags.filter(article => article.tags.includes(this.tag))
-    }
-  },
-  components: {
-    "article-view": articleView
-  },
   methods: {
-    showArticle(event) {
-      if (event.target.nodeName !== "SPAN") {
-        let target = event.target.querySelector("span").innerText;
-        target = this.taggedContent.find(e => e.url == target);
-        this.viewArticle = {
-          title: target.title,
-          link: target.url,
-          author: "",
-          date: "",
-          img: ""
-        }
-      }
-    },
-    otherTags(article) {
-      return this.allTags
-        .find(a => a.url == article.url).tags
-        .filter(tag => tag != this.tag)
-    },
-    changeTag(tag) {
-      this.$root.$emit("showTag",tag);
-    },
-    hideArticle() {
-      this.viewArticle = false;
-    },
-    async getTags() {
-      this.allTags = await this.$root.spekti.tags.allContent;
+    removeTag(url) {
+      this.$store.dispatch("tagArticle",{ url , tag: this.tag });
     }
-  },
-  created() {
-    this.$root.$on("showTag",this.hideArticle);
-    this.$on("hideArticle",this.hideArticle);
-  },
-  mounted() {
-    this.getTags()
   },
   props: ["tag"],
 };
