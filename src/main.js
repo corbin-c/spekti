be8ba10f4bb1fe52f1b1dcc99e025fdc1170f619
Clément Corbin <corbin.clement@gmail.com>
2021-04-28 14:43:29 +0200

    vuex & router

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import Vue from "vue"
 import App from "./App.vue"
+import router from "./router"
+import store from "./store"
 
 Vue.config.productionTip = false
 
 new Vue({
-  data: function() {
-    return {
-    logged: false,
-    spekti: false,
-    scrollY: 0
-  }
-  },
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount("#app")
