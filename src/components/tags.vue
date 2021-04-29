dd2219d4d40ab39d42bb8e503998caf87de90044
Clément Corbin <corbin.clement@gmail.com>
2021-04-29 13:51:51 +0200

    store / router

--- src/components/tags.vue
+++ src/components/tags.vue
@@ -1,33 +1,10 @@
 <template>
   <div class="modal-body d-flex justify-content-around flex-wrap align-content-around">
-    <p v-if="allTags.length == 0">There is no tag yet. Start reviewing or tagging articles to see something here.</p>
-    <router-link v-else class="btn btn-outline-secondary my-2" v-for="tag in allTags" :key="tag" :to="'/tag/' + tag">{{tag}}</router-link>
+    <p v-if="$store.getters.allTags.length == 0">There is no tag yet. Start reviewing or tagging articles to see something here.</p>
+    <router-link v-else class="btn btn-outline-secondary my-2 mx-1" v-for="tag in $store.getters.allTags" :key="tag" :to="'/tag/' + tag">{{tag}}</router-link>
   </div>
 </template>
 <script>
 export default {
-  data: function() {
-    return {
-      tags: [],
-    }
-  },
-  computed: {
-    allTags() {
-      return this.tags;
-    },
-  },
-  methods: {
-    async getTags() {
-      await this.$root.spekti.ready;
-      this.tags = await this.$root.spekti.tags.allTags;
-    },
-    showTag(tag) {
-      this.$root.$emit("showTag",tag);
-      this.$parent.close();
-    }
-  },
-  mounted: function() {
-    this.getTags();
-  },
 };
 </script>
