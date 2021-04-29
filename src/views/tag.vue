f6e8d11c401150c7e275782ce50f054b3e0c0beb
Clément Corbin <corbin.clement@gmail.com>
2021-04-29 15:57:41 +0200

    store / router

--- src/views/tag.vue
+++ src/views/tag.vue
@@ -5,12 +5,14 @@
       <p class="card-body card-text"  v-if="$store.getters.taggedContent(tag).length == 0">There is no content for this tag yet. Start reviewing or tagging articles to see something here.</p>
       <ul class="list-group list-group-flush" v-else>
         <li class="list-group-item d-flex justify-content-between align-items-center" v-for="article in $store.getters.taggedContent(tag)" :key="article.url">
-          <router-link
+          <commit-link
             class="text-secondary text-decoration-none"
-            :to="article.url"
+            to="/article/"
+            statekey="currentArticle"
+            :payload="{ link: article.url }"
             :title="'Read this article: '+article.title">
               {{ article.title }}
-          </router-link>
+          </commit-link>
           <router-link
             class="badge badge-info badge-pill"
             v-for="otherTag in $store.getters.otherTags(article.url,tag)"
@@ -28,12 +30,21 @@
   </section>
 </template>
 <script>
-
+import commitLink from "@/components/commit-link.vue";
 export default {
+  mounted() {
+    this.$root.$emit("hideModal");    
+  },
+  updated() {
+    this.$root.$emit("hideModal");
+  },
   methods: {
     removeTag(url) {
       this.$store.dispatch("tagArticle",{ url , tag: this.tag });
     }
+  },
+  components: {
+    "commit-link": commitLink
   },
   props: ["tag"],
 };
