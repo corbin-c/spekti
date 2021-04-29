bf17a56f16863037573a0d5983cdef71b0df3182
Clément Corbin <corbin.clement@gmail.com>
2021-04-29 13:51:13 +0200

    use store

--- src/components/tagbar.vue
+++ src/components/tagbar.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="card-header sticky-top tagbar d-flex justify-content-around">
-    <button class="btn btn-outline-secondary" v-if="isFav" v-on:click="removeTag('fav')" title="unfav this article">
-      <svg class="d-inline">
+    <button class="btn btn-outline-secondary" v-on:click="tag('fav')" :title="(isFav ? 'un':'')+'fav this article'">
+      <svg class="d-inline" v-if="!isFav">
+        <use xlink:href="/octicons-sprite/octicons-sprite.svg#bookmark-24"></use>
+      </svg>
+      <svg class="d-inline" v-else>
         <use xlink:href="/octicons-sprite/octicons-sprite.svg#bookmark-fill-24" class="regular"></use>
         <use xlink:href="/octicons-sprite/octicons-sprite.svg#bookmark-slash-fill-24" class="hover"></use>
       </svg>
     </button>
-    <button class="btn btn-outline-secondary" v-else v-on:click="addTag('fav')" title="fav this article">
-      <svg class="d-inline">
-        <use xlink:href="/octicons-sprite/octicons-sprite.svg#bookmark-24"></use>
-      </svg>
-    </button>
-    <button class="btn btn-outline-secondary" v-if="isReviewed" v-on:click="removeTag('reviewed')" title="mark this article as unseen">
-      <svg class="d-inline">
+    <button class="btn btn-outline-secondary" v-on:click="tag('reviewed')" :title="'mark this article as '+(isReviewed ? 'un':'')+'seen'">
+      <svg class="d-inline" v-if="isReviewed">
         <use xlink:href="/octicons-sprite/octicons-sprite.svg#eye-slash-24"></use>
       </svg>
-    </button>
-    <button class="btn btn-outline-secondary" v-else v-on:click="addTag('reviewed')" title="mark this article as seen">
-      <svg class="d-inline">
+      <svg class="d-inline" v-else>
         <use xlink:href="/octicons-sprite/octicons-sprite.svg#eye-24"></use>
       </svg>
     </button>
@@ -35,49 +31,27 @@
 </template>
 <script>
 export default {
-  data: function() {
-    return {
-      allTags: [],
-    }
-  },
   computed: {
+    tags() {
+      return this.$store.getters.tagsAbout(this.url).tags || [];
+    },
     isFav() {
-      return this.allTags.includes("fav");      
+      return this.tags.includes("fav");
     },
     isReviewed() {
-      return this.allTags.includes("reviewed");
+      return this.tags.includes("reviewed");
     }
   },
   methods: {
-    async getTags() {
-      let tags = await this.$root.spekti.tags.tagsAbout(this.url);
-      if (typeof tags !== "undefined") {
-        this.allTags = tags.tags;        
-      } else {
-        this.allTags = [];
-      }
-    },
     showTags() {
-      this.$root.$emit("showModal","tags-about");
-      this.$root.$emit("modalDetails",{url: this.url, title: this.title});
+      this.$root.$emit("showModal", { component: "tags-about", details: { url: this.url, title: this.title } });
     },
     showNotes() {
-      this.$root.$emit("showModal","notes-about");
-      this.$root.$emit("modalDetails",this.url);
+      this.$root.$emit("showModal", { component: "notes-about", details: { url: this.url } });
     },
-    async addTag(tag) {
-      await this.$root.spekti.tags.tagArticle(this.url,this.title,tag);
-      this.$root.$emit("update");
-      this.getTags();
+    tag(tag) {
+      this.$store.dispatch("tagArticle", { url: this.url, title: this.title, tag });
     },
-    async removeTag(tag) {
-      await this.$root.spekti.tags.untagArticle(this.url,tag);
-      this.$root.$emit("update");
-      this.getTags();
-    }
-  },
-  mounted() {
-    this.getTags();
   },
   props: ["url","title"],
 };
