7d3108041ef57257bc130e9063a7b98fd1330f61
Clément Corbin <corbin.clement@gmail.com>
2021-04-29 13:49:00 +0200

    router link to dispatch state & events

---
+++ src/components/commit-link.vue
@@ -0,0 +1,20 @@
+<template>
+  <a :href="to" :title="title" v-on:click="navigate"><slot></slot></a>
+</template>
+<script>
+export default {
+  methods: {
+    navigate(e) {
+      e.preventDefault();
+      if (this.statekey && this.payload) {
+        this.$store.commit("fill", { key: this.statekey, content: this.payload });
+      }
+      if (this.event) {
+        this.$root.$emit(this.event);
+      }
+      this.$router.push({ path: this.to });
+    }
+  },
+  props: ["to","title","statekey","payload","event"]
+}
+</script>
